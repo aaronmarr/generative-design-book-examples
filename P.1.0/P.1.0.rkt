efc7952809d2528a9b0b07cf990e2794ba5d2eef
Aaron Marr <aaron.marr@icloud.com>
2019-12-27 21:44:07 +0000

    Initial commit

---
+++ P.1.0/P.1.0.rkt
@@ -0,0 +1,48 @@
+#lang racket
+
+(require colors)
+(require racket/draw)
+
+
+(define target (make-bitmap 720 720))
+(define dc (new bitmap-dc% [bitmap target]))
+
+(define angle-in-deg (random 360))
+
+(define bg-size 720)
+(define fg-size (random bg-size))
+(define offset (/ (- bg-size fg-size) 2))
+
+(define bg-color
+  (make-object
+   color%
+   (hsl->color
+    (hsl
+     (/ angle-in-deg 360)
+     0.5
+     0.5))))
+
+(define fg-color
+  (make-object
+   color%
+   (hsl->color
+    (hsl
+     (/ ((lambda (deg)
+           (if (<= deg 179)
+               (+ deg 180)
+               (- deg 180))) angle-in-deg) 360)
+     0.5
+     0.5))))
+
+(send dc set-pen "white" 1 'transparent)
+(send dc set-brush bg-color 'solid)
+(send dc draw-rectangle
+      0 0
+      bg-size bg-size)
+
+(send dc set-brush fg-color 'solid)
+(send dc draw-rectangle
+      offset offset
+      fg-size fg-size)
+
+(send target save-file "test-file.png" 'png)
---
+++ P.1.1.1/P.1.1.1.rkt
@@ -0,0 +1,31 @@
+#lang racket
+
+(require colors)
+(require racket/draw)
+
+
+(define width 800)
+(define height 400)
+
+(define target (make-bitmap width height))
+(define dc (new bitmap-dc% [bitmap target]))
+
+(define step-x (/ 360 4))
+(define step-y (/ 360 8))
+
+(send dc set-pen "white" 1 'transparent)
+
+(for ([grid-y (range 0 400 step-y)])
+  (for ([grid-x (range 0 800 step-x)])
+    (define fill-color
+      (make-object
+       color%
+       (hsl->color
+        (hsl
+         (/ grid-x width)
+         (/ (- height grid-y) height)
+         0.5))))
+    (send dc set-brush fill-color 'solid)
+    (send dc draw-rectangle grid-x grid-y step-x step-y)))
+
+(send target save-file "test-file.png" 'png)
